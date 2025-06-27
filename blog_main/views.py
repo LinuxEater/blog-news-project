5eb92a07b9cf4702ca4d478f65f7ca69970c5203
LinuxEater <moisessouzasantos001@gmail.com>
2025-06-27 03:08:34 -0300

    make some modifications at front end and backend

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,7 +4,11 @@ from blogs.models import Blog, Category
 
 def home(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')[:3]
+    posts = Blog.objects.filter(is_featured=False, status='Published')
     context = {
         'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts,
         }
     return render(request, 'home.html', context)
