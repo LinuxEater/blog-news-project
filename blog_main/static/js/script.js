7dba25a5b64fd3aefa93475b95f946c0fc9a570e
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-02 11:06:42 -0300

    testing js files

--- blog_main/static/js/script.js
+++ blog_main/static/js/script.js
@@ -1,1 +1,9 @@
-//expanding cards
+//expanding card
+
+const panel = document.querySelector('.panel');
+
+console.log(panel);
+
+panel.addEventListener('click', () => {
+    alert('Panel clicked!');
+});
