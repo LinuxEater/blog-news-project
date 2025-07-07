649ffc3a619711a71eac417a90264060181d507a
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-07 16:46:37 -0300

    Fix background image display using .url in template

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -118,10 +119,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [ 'blog_main/static',]  # Custom static files directory]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'  # Directory to store uploaded media files
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')# Directory to store uploaded media files
--- blog_main/views.py
+++ blog_main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from blogs.models import Blog, Category
 
 def home(request):
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')[:3]
     posts = Blog.objects.filter(is_featured=False, status='Published')
     context = {
         'categories': categories,
