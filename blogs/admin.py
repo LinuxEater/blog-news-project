f51a15b4a8b19e4554fc7148b38e0ec2ba33cb8f
LinuxEater <moisessouzasantos001@gmail.com>
2025-07-15 12:24:19 -0300

    feat: add search custom list to adm panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -7,9 +7,15 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'category', 'author', 'status', 'is_featured', 'created_at', 'author')
     search_fields = ('title', 'slug', 'category__category_name', 'author')
     list_editable = ('is_featured',)
+    list_filter = ('status', 'category', 'created_at', 'author')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'created_at', 'updated_at')
+    search_fields = ('category_name',)
+    list_filter = ('created_at', 'updated_at')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Blog, BlogAdmin)
 
 
